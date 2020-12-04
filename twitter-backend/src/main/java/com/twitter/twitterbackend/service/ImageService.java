f84f31e8a4a5ba60e758de0a2daf7ced9c65b782
rishijatania <jatania.r@husky.neu.edu>
2020-12-03 20:30:37 -0500

    Organizing Imports

--- twitter-backend/src/main/java/com/twitter/twitterbackend/service/ImageService.java
+++ twitter-backend/src/main/java/com/twitter/twitterbackend/service/ImageService.java
@@ -5,8 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
