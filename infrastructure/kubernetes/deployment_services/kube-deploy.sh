92a201f4ec33691e67d15497ac172cd0c7babfed
rishijatania <jatania.r@husky.neu.edu>
2020-12-12 23:01:31 -0500

    shell scripts

---
+++ infrastructure/kubernetes/deployment_services/kube-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+kubectl apply -f twitter-backend-secrets.yml &&
+kubectl apply -f twitter-backend-config.yml &&
+kubectl apply -f twitter-backend-deployment.yml &&
+kubectl apply -f service-twitter-backend-lb.yml &&
+kubectl apply -f twitter-tweet-config.yml &&
+kubectl apply -f twitter-tweet-deployment.yml && 
+kubectl apply -f service-twitter-tweet-lb.yml &&
+kubectl apply -f twitter-queue-deployment.yml &&
+kubectl apply -f service-twitter-queue-lb.yml &&
+kubectl apply -f twitter-middleware-secrets.yml &&
+kubectl apply -f twitter-middleware-config.yml &&
+kubectl apply -f twitter-middleware-deployment.yml &&
+kubectl apply -f service-twitter-middleware-lb.yml
